692fcd47d2dbbd3d3df896c45d4428bf07fe3a1e
kenshin <tanaka.aa@icloud.com>
2020-11-23 22:49:50 +0900

    added isShow button

--- ADG/AtcoderDevotionGraph.js
+++ ADG/AtcoderDevotionGraph.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtcoderDevotionGraph
 // @namespace    http://atcoder.jp/
-// @version      0.1
+// @version      0.2.0β
 // @description  Overlay your devoiting graph on your rating graph
 // @author       kemkemG0
 // @include      *://atcoder.jp/users*
@@ -18,59 +18,36 @@
 
     //##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
 
-    (() => {
+    let scriptsArray = $('script');//<script>タグのものを配列に突っ込む
+    scriptsArray[14].remove(); // 対象のタグを消す記述 x[14]がグラフを読み込むjs
 
-        let scriptsArray = $('script');//<script>タグのものを配列に突っ込む
-        scriptsArray[14].remove(); // 対象のタグを消す記述 x[14]がグラフを読み込むjs
+    //なんでこれ必要？？  -->>一度読み込んだscriptタグはDOMから消しても効果は残るからそれを消すため
+    let copyPage = $("html").clone().html(); // 対象のタグが消えたページをコピー
+    $("html").remove(); // ページをまるごと削除
+    document.write(copyPage); // コピーしてあったページ内容をペースト
 
-        //なんでこれ必要？？  -->>一度読み込んだscriptタグはDOMから消しても効果は残るからそれを消すため
-        let copyPage = $("html").clone().html(); // 対象のタグが消えたページをコピー
-        $("html").remove(); // ページをまるごと削除
-        document.write(copyPage); // コピーしてあったページ内容をペースト
-
-    })()
+    const element = document.getElementsByClassName('btn-text-group')[document.getElementsByClassName('btn-text-group').length - 1];
+    const insertButton = Object.assign(document.createElement('button'), {
+        className: '',
+        id: 'shoujinButtonID',
+        style: '\
+    margin-left:50px;\
+    appearance: none;\
+    border: 0;\
+    border-radius: 5px;\
+    background: #20b2aa;\
+    color: #fff;\
+    padding: 8px 16px;\
+    font-size: 16px;\
+    '
+    }
+    );
+    insertButton.textContent = "精進グラフの表示を切り替える"
+    element.appendChild(insertButton)
+    console.log(element)
 
 
     //##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
-
-
-
-    const username = document.getElementsByClassName("username")[0].textContent;
-
-    const res = await fetch("https://kenkoooo.com/atcoder/atcoder-api/results?user=" + username);
-    let allJson = await res.json()
-
-    let devoting_rating_history = []
-
-    for (let i = 0; i < allJson.length; i++) {
-        if (allJson[i].result == 'AC' && allJson[i].point <= 3000) devoting_rating_history.push({ ...allJson[i] });
-    }
-    function compare(a, b) { return a.epoch_second - b.epoch_second; }//比較関数
-    devoting_rating_history.sort(compare);//時間順にソート
-
-
-    for (let i = 0; i < devoting_rating_history.length - 1; i++) {//合計の累積和的な
-        devoting_rating_history[i + 1].point += devoting_rating_history[i].point;
-        devoting_rating_history[i].point /= 100;
-    }
-    devoting_rating_history[devoting_rating_history.length - 1].point /= 100;
-    //今までの累積和/100　が高さ
-
-    console.log(devoting_rating_history);
-
-
-
-
-
-
-    $(window).load(init);//ページの構成を全部把握してからinit()実行
-
-
-    //init()
-
-    // window.alert('22222');
-
-    //クリエイトjsとやらをつかっている
 
     // const
     const MARGIN_VAL_X = 86400 * 30;
@@ -134,6 +111,40 @@
     let rating_text, place_text, diff_text, date_text, contest_name_text;
     let particles;
     let standings_url;
+    const username = document.getElementsByClassName("username")[0].textContent;
+
+    const res = await fetch("https://kenkoooo.com/atcoder/atcoder-api/results?user=" + username);
+    let allJson = await res.json()
+    let devoting_rating_history = []
+    for (let i = 0; i < allJson.length; i++) {
+        if (allJson[i].result == 'AC' && allJson[i].point <= 3000) devoting_rating_history.push({ ...allJson[i] });
+    }
+    function compare(a, b) { return a.epoch_second - b.epoch_second; }//比較関数
+    devoting_rating_history.sort(compare);//時間順にソート
+
+    for (let i = 0; i < devoting_rating_history.length - 1; i++) {//合計の累積和的な
+        devoting_rating_history[i + 1].point += devoting_rating_history[i].point;
+        devoting_rating_history[i].point /= 100;
+    }
+    devoting_rating_history[devoting_rating_history.length - 1].point /= 100;
+    //今までの累積和/100　が高さ
+
+
+    window.addEventListener('load', (event) => {
+        init();
+        let shoujinButtonID = document.getElementById('shoujinButtonID');
+        shoujinButtonID.addEventListener('click', function () {
+            devoting_chart_container.visible = !devoting_chart_container.visible;
+            stage_graph.update();
+        });
+    });
+
+    console.log(devoting_rating_history.length);
+
+
+    //クリエイトjsとやらをつかっている
+
+
 
 
     //いい感じにキャンバスの大きさを設定してマウスオーバーもONにする
@@ -215,7 +226,7 @@
         devoting_x_max = 0;
         devoting_y_min = 10000;
         devoting_y_max = 0;
-        for (let i = 0; i < n; i++) {
+        for (let i = 0; i < devoting_rating_history.length; i++) {
             devoting_x_min = Math.min(devoting_x_min, devoting_rating_history[i].epoch_second);
             devoting_x_max = Math.max(devoting_x_max, devoting_rating_history[i].epoch_second);
             devoting_y_min = Math.min(devoting_y_min, devoting_rating_history[i].point);
@@ -227,7 +238,7 @@
         devoting_y_max += MARGIN_VAL_Y_HIGH;
 
 
-        //形を決める まだ改善の余地あり
+        //形を決める 
         y_min = Math.min(y_min, devoting_y_min);
         y_max = Math.max(y_max, devoting_y_max);
         x_min = Math.min(x_min, devoting_x_min);
@@ -434,61 +445,48 @@
     function initDevotingChart() {
         devoting_chart_container = new cj.Container();//コンテナでまとめると、同時に動かせたりして良い
         stage_graph.addChild(devoting_chart_container);//これは devoting_じゃない
-        devoting_chart_container.shadow = new cj.Shadow("rgba(0,0,0,0.1)", 1, 2, 3);//チャートの下に影
+        devoting_chart_container.shadow = new cj.Shadow("rgba(0,0,0,0.3)", 1, 2, 3);//チャートの下に影
 
         devoting_line_shape = newShape(devoting_chart_container);
         devoting_highest_shape = newShape(devoting_chart_container);
         devoting_vertex_shapes = new Array();
 
         //マウスおいたら丸が大きくなるやつ
-        // function mouseoverVertex(e) {
-        //     vertex_shapes[e.target.i].scaleX = vertex_shapes[e.target.i].scaleY = 1.2;
-        //     stage_graph.update();
-        //     setStatus(rating_history[e.target.i], true);
-        // };
-        // function mouseoutVertex(e) {
-        //     vertex_shapes[e.target.i].scaleX = vertex_shapes[e.target.i].scaleY = 1;
-        //     stage_graph.update();
-        // };
+        function devoting_mouseoverVertex(e) {
+            devoting_vertex_shapes[e.target.i].scaleX = devoting_vertex_shapes[e.target.i].scaleY = 2;
+            stage_graph.update();
+        };
+        function devoting_mouseoutVertex(e) {
+            devoting_vertex_shapes[e.target.i].scaleX = devoting_vertex_shapes[e.target.i].scaleY = 1;
+            stage_graph.update();
+        };
 
-        // let highest_i = 0;
-        // for (let i = 0; i < n; i++) {
-        //     if (rating_history[highest_i].point < rating_history[i].point) {
-        //         highest_i = i;
-        //     }
-        // }
+
         //historyの数だけ配列にpushしてイベントリスナーも設定
         for (let i = 0; i < devoting_n; i++) {
             devoting_vertex_shapes.push(newShape(devoting_chart_container));
             devoting_vertex_shapes[i].graphics.beginStroke("#FFF");
-            //if (i == highest_i) vertex_shapes[i].graphics.s("#000");
-            devoting_vertex_shapes[i].graphics.setStrokeStyle(0.1).beginFill(getColor(devoting_rating_history[i].point)[1]).dc(0, 0, 1.5);
+            if (i == devoting_n - 1) {
+                devoting_vertex_shapes[i].graphics.s("#000");
+                devoting_vertex_shapes[i].graphics.setStrokeStyle(1).beginFill(getColor(devoting_rating_history[i].point)[1]).dc(0, 0, 2.5);
+            }
+            else {
+                devoting_vertex_shapes[i].graphics.setStrokeStyle(0.5).beginFill(getColor(devoting_rating_history[i].point)[1]).dc(0, 0, 2);
+            }
             devoting_vertex_shapes[i].x = OFFSET_X + PANEL_WIDTH * getPer(devoting_rating_history[i].epoch_second, x_min, x_max);//devotingじゃないほうに合わせる？
             devoting_vertex_shapes[i].y = OFFSET_Y + (PANEL_HEIGHT - PANEL_HEIGHT * getPer(devoting_rating_history[i].point, y_min, y_max));
             devoting_vertex_shapes[i].i = i;
             let hitArea = new cj.Shape();
             hitArea.graphics.f("#000").dc(1.5, 1.5, 6);
             devoting_vertex_shapes[i].hitArea = hitArea;
-            // devoting_vertex_shapes[i].addEventListener("mouseover", mouseoverVertex);
-            // devoting_vertex_shapes[i].addEventListener("mouseout", mouseoutVertex);
+            devoting_vertex_shapes[i].addEventListener("mouseover", devoting_mouseoverVertex);
+            devoting_vertex_shapes[i].addEventListener("mouseout", devoting_mouseoutVertex);
         }
 
-        // {//highest 関連
-        //     let dx = 80;
-        //     if ((x_min + x_max) / 2 < rating_history[highest_i].epoch_second) dx = -80;
-        //     let x = vertex_shapes[highest_i].x + dx;
-        //     let y = vertex_shapes[highest_i].y - 16;
-        //     highest_shape.graphics.s("#FFF").mt(vertex_shapes[highest_i].x, vertex_shapes[highest_i].y).lt(x, y);
-        //     highest_shape.graphics.s("#888").f("#FFF").rr(x - HIGHEST_WIDTH / 2, y - HIGHEST_HEIGHT / 2, HIGHEST_WIDTH, HIGHEST_HEIGHT, 2);
-        //     highest_shape.i = highest_i;
-        //     let highest_text = newText(stage_graph, x, y, "12px Lato");
-        //     highest_text.text = "Highest: " + rating_history[highest_i].point;
-        //     highest_shape.addEventListener("mouseover", mouseoverVertex);
-        //     highest_shape.addEventListener("mouseout", mouseoutVertex);
-        // }
+
 
         //チャートの線関連
-        devoting_line_shape.graphics.s("#afb5b8").ss(1);
+        devoting_line_shape.graphics.s("white").ss(1);
         devoting_line_shape.graphics.mt(devoting_vertex_shapes[0].x, devoting_vertex_shapes[0].y);
         for (let i = 0; i < devoting_rating_history.length; i++) {
             devoting_line_shape.graphics.lt(devoting_vertex_shapes[i].x, devoting_vertex_shapes[i].y);
